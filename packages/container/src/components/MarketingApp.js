f2ed54431bc3c8ac6533957734ccaaf9ffed30ad
Josh Kemp <josh@jsmithmedia.tech>
2023-02-25 22:09:04 -0500

    created container workflow

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -4,9 +4,7 @@ import React, { useRef, useEffect } from 'react';
 export default () => {
     const ref = useRef(null);
 
-    useEffect(() => {
-        mountMarketing(ref.current);
-    }, []);
+    useEffect(() => mountMarketing(ref.current), []);
 
     return <div ref={ref} />;
 };
